bf962ee236e13fe7fc9d74c3ec61f306347d44ad
grzegorzxpatyk <70432509+grzegorzxpatyk@users.noreply.github.com>
2023-06-28 12:19:42 +0200

    import bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import App from './App';
 
